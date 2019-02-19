a02fb2612697cc0d9ae642bdce79d1d46d2b699e
GC Jyothi Prasanna <43312457+JyothiPrasanna768@users.noreply.github.com>
2019-02-19 12:22:13 +0900

    Add files via upload

---
+++ Lab5/IDTFT.py
@@ -0,0 +1,32 @@
+import cmath as cm
+from matplotlib import pyplot as plt
+import numpy as np
+import scipy.integrate as integrate
+from scipy.integrate import quad
+import scipy.special as special\
+
+def integrand(w,x,e):
+    return x[w]*e
+
+def dtft(xw,N):
+    x=[]
+    j=cm.sqrt(-1)
+    n=len(x)
+    #w=np.linspace(0,2*np.pi,N)
+    for i in range(0,n):
+        sum=0
+        for w in range(0,2*np.pi,1/N):
+            x=X[k]
+            e=np.exp(j*w*i)
+            I=quad(integrand,0,2*np.pi,args=(x,e)
+
+        x.append(sum)
+      #xw=sum
+    
+    plt.plot(w,np.abs(x))
+    return(xw)
+xw=np.array(input("enter x[n] value"))
+dtft(x,10)
+plt.xlabel("frequency")
+plt.ylabel("|x[w]|")
+plt.show()
