cf72aa14678367862f51b2e740f44289bd6cac0e
GC Jyothi Prasanna <43312457+JyothiPrasanna768@users.noreply.github.com>
2019-02-25 15:23:43 +0900

    Create DFT.py

---
+++ Lab6/DFT.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cmath as cm
+import matplotlib.pyplot as plt 
+
+def DFT(x,N):
+	i=len(x) # size of x
+	print(i)
+	j=cm.sqrt(-1)
+	X=[]
+	#w=np.linspace(0,2*np.pi,N)
+	for k in range(N):
+		sum=0
+		for n in range(i):
+			sum=sum+(x[n]*(cm.exp(((-j)*2*cm.pi*n*k)/N)))
+		X.append(sum)
+	
+	print(X)
+	plt.subplot(1,2,1)
+	plt.stem(np.abs(X))
+	plt.title("magnitude of X[w]")
+	plt.xlabel("frequency")
+	plt.ylabel("|X[w]|")
+	plt.subplot(1,2,2) 
+	plt.stem(np.angle(X))
+	plt.title("phase of X[w]")
+	plt.xlabel("frequency")
+	plt.ylabel("/_X[w]")
+	return(X)
+
+N=int(input("enter no. of samples"))  #choose 8-point or 4-point dft
+x=np.array(input("enter values of x[n]"))
+DFT(x,N)
+
+plt.show()
+
